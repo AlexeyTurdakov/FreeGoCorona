8dfed03e2ed246cc8293c00a4f2e8691e432ec27
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-09-01 21:57:47 +0400

    +expertsBlock, getStarted, staySelf

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 "use strict";
 
+let users = document.getElementById("users");
+let experts = document.getElementById("experts");
 
+let usersNum = +users.innerText;
+let expertsNum = +experts.innerText;
+
+function changeNumberUsers() {
+  users.innerText = `${++usersNum}`;
+}
+
+function changeExpertsUsers() {
+  experts.innerText = `${++expertsNum}`;
+}
+
+setInterval(changeNumberUsers, 300, usersNum);
+setInterval(changeExpertsUsers, 1500, expertsNum);
